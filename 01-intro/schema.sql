648cdd7d12b75817215c2a39292550c926a93f2d
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-12-17 15:55:11 -0400

    Create schema.sql

---
+++ 01-intro/schema.sql
@@ -0,0 +1,23 @@
+
+/*
+SQL stands for Structured Query Language.
+SQL is a standard language for storing, manipulating and retrieving data in databases.
+Most of the SQL database programs also have their own proprietary extensions in addition to the SQL standard.
+*/
+
+/*
+To build a web site that shows data from a database, you will need:
+    ->  An (RDBMS) database program (i.e. MS Access, SQL Server, MySQL);
+    ->  To use a server-side scripting language, like PHP or ASP;
+    ->  To use SQL to get the data you want;
+    ->  To use HTML / CSS to style the page.
+*/
+
+/*
+Relational Database Management System is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+The Database structure:
+    ->  The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+    ->  Every table is broken up into smaller entities called fields. A field is a column in a table that is designed to maintain specific information about every record in the table.
+    ->  A record, also called a row, is each individual entry that exists in a table. A record is a horizontal entity in a table.
+    ->  A column is a vertical entity in a table that contains all information associated with a specific field in a table.
+*/
