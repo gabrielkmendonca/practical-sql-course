33ad2a40dc13ca7738cbf8a4159e304ad4a0c63c
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-12-18 17:43:02 -0400

    Update schema.sql

--- 02-statements/schema.sql
+++ 02-statements/schema.sql
@@ -34,32 +34,6 @@ Inside a table, a column often contains many duplicate values; and sometimes you
 
 
 
-SQL ORDER BY keyword
-/*
-The ORDER BY keyword is used to sort the result-set in ascending or descending order.
-Syntax:
-
-    SELECT column1, column2, ... FROM table_name 
-    ORDER BY column1, column2, ... ASC|DESC;
-
-    ->  ASC (default) — sort the records in ascending order
-    ->  DESC — sort the records in descending order
-*/
-
-    SELECT * FROM Products 
-    ORDER BY Price;
-
-/*
-ORDER BY several columns:
-    - selects all customers from the "Customers" table, sorted ascending by the "Country" and descending by the "CustomerName"
-*/
-
-    SELECT * FROM Customers 
-    ORDER BY Country ASC, 
-    CustomerName DESC;
-
-
-
 SQL INSERT INTO
 /*
 The INSERT INTO statement is used to insert new records in a table.
