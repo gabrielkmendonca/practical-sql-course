d92974f5a3dac25e8f636c6741a47dc0eecc7db5
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-12-18 17:32:52 -0400

    Create schema.sql

---
+++ 02-statements/schema.sql
@@ -0,0 +1,151 @@
+SQL SELECT
+/*
+The SELECT statement is used to select data from a database.
+Syntax:
+
+    SELECT column1, column2, ... FROM table_name;
+
+    ->  column1, column2, ... are the field names of the table you want to select data from;
+    ->  table_name represents the name of the table you want to select data from.
+*/
+
+    SELECT CustomerName, City FROM Customers;
+
+/*
+Select all columns:
+    ->  to return all columns, without specifying every column name, use the "SELECT *" syntax
+*/
+
+    SELECT * FROM Customers;
+
+
+
+SQL SELECT DISTINCT 
+/*
+The SELECT DISTINCT statement is used to return only distinct (different) values.
+Syntax:
+
+    SELECT DISTINCT column1, column2, ... FROM table_name;
+
+Inside a table, a column often contains many duplicate values; and sometimes you only want to list the different (distinct) values.
+*/ 
+
+    SELECT DISTINCT Country FROM Customers;
+
+
+
+SQL ORDER BY keyword
+/*
+The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+Syntax:
+
+    SELECT column1, column2, ... FROM table_name 
+    ORDER BY column1, column2, ... ASC|DESC;
+
+    ->  ASC (default) — sort the records in ascending order
+    ->  DESC — sort the records in descending order
+*/
+
+    SELECT * FROM Products 
+    ORDER BY Price;
+
+/*
+ORDER BY several columns:
+    - selects all customers from the "Customers" table, sorted ascending by the "Country" and descending by the "CustomerName"
+*/
+
+    SELECT * FROM Customers 
+    ORDER BY Country ASC, 
+    CustomerName DESC;
+
+
+
+SQL INSERT INTO
+/*
+The INSERT INTO statement is used to insert new records in a table.
+Syntax:
+
+    1 ->    Specify both the column names and the values to be inserted:
+    
+            INSERT INTO table_name (column1, column2, column3, ...)
+            VALUES (value1, value2, value3, ...);
+
+    2 ->    If adding values for all the columns of the table, does not need to specify the column names in the SQL query:
+    
+            INSERT INTO table_name
+            VALUES (value1, value2, value3, ...);
+
+To insert multiple rows of data in one statement, use the same INSERT INTO with multiple values.
+The VALUES defines the values to be inserted into the table.
+*/
+
+    INSERT INTO Customers (CustomerName, City, Country)
+    VALUES ('Cardinal', 'Stavanger', 'Norway');
+
+    INSERT INTO Customers
+    VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+           ('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+           ('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
+
+
+
+SQL UPDATE
+/*
+The UPDATE statement is used to modify the existing records in a table.
+Syntax:
+
+    UPDATE table_name
+    SET column1 = value1, column2 = value2, ...
+    WHERE condition;
+
+The SET defines the values to be inserted.
+The WHERE clause specifies which record(s) that should be updated. If you omit it, all records in the table will be updated.
+*/
+
+    UPDATE Customers
+    SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+    WHERE CustomerID = 1;
+
+/*
+UPDATE Multiple Records:
+    ->   the WHERE clause that determines how many records will be updated. Creating a condition that filters many records, will update all of them. 
+*/
+
+    UPDATE Customers
+    SET ContactName='Juan'
+    WHERE Country='Mexico';
+
+/*
+    ->  If the WHERE clause is not used, the entire table will be updated.
+*/
+
+    UPDATE Customers
+    SET ContactName='Juan';
+
+
+
+SQL DELETE
+/*
+The DELETE statement is used to delete existing records in a table.
+Syntax:
+
+    DELETE FROM table_name 
+    WHERE condition;
+*/
+
+    DELETE FROM Customers 
+    WHERE CustomerName='Alfreds Futterkiste';
+
+/*
+DELETE all records:
+    ->  It is possible to delete all rows in a table without deleting the table structure.
+*/
+
+    DELETE FROM table_name;
+
+
+
+SQL DROP TABLE statement
+/*
+-----------------------------------------------------------------------------------------------------------------
+*/
